b31ae258cc125f7f3424e6f47968e6c58f924ff8
Anton Zhilinskey <zhilinskiianton@gmail.com>
2023-09-12 13:27:13 +0700

    Fixed launch bug

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from auth.base_config import auth_backend, fastapi_users
-from auth.schemas import UserRead, UserCreate
+from src.auth.base_config import auth_backend, fastapi_users
+from src.auth.schemas import UserRead, UserCreate
 # from operations.router import router as router_operation
 
 app = FastAPI(title="Trading App")
