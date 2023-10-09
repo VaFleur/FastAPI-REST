0b73fad31e4f4b5dd36fe2ad9dbb109ca20a84a5
Anton Zhilinskey <zhilinskiianton@gmail.com>
2023-10-09 14:53:20 +0700

    Added new directory structure

--- src/custom_exception.py
+++
@@ -1,7 +0,0 @@
-class ExecutionException(Exception):
-    def __init__(self, error: str, message: str, http_code: int = 400):
-        super().__init__(error)
-
-        self.error = error
-        self.message = message
-        self.http_code = http_code
