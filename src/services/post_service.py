071406dfe41f7eaf2f489f0888a68797b9496242
Anton Zhilinskey <zhilinskiianton@gmail.com>
2023-10-23 13:53:05 +0700

    Added PostService

--- src/services/post_service.py
+++ src/services/post_service.py
@@ -1,16 +1,45 @@
-from src.schemas.post_schema import PostSchemaAdd
+from src.schemas.post_schema import PostSchemaAdd, PostSchemaEdit, PostHistorySchemaAdd
 from src.utils.repository import AbstractRepository
+from src.utils.unit_of_work import IUnitOfWork
 
 
 class PostService:
-    def __init__(self, repo: AbstractRepository):
-        self.repo: AbstractRepository = repo
+    @staticmethod
+    async def add_post(uow: IUnitOfWork, data: PostSchemaAdd):
+        data_dict = data.model_dump()
+        async with uow:
+            post_id = await uow.posts.add_one(data_dict)
+            await uow.commit()
+            return post_id
 
-    async def add_post(self, schema: PostSchemaAdd):
-        post_dict = schema.model_dump()
-        post_id = await self.repo.add_one(post_dict)
-        return post_id
+    @staticmethod
+    async def get_posts(uow: IUnitOfWork):
+        async with uow:
+            posts = await uow.posts.find_all()
+            return posts
 
-    async def get_posts(self):
-        posts = await self.repo.find_all()
-        return posts
+    @staticmethod
+    async def edit_posts(uow: IUnitOfWork, post_id: int, data: PostSchemaEdit):
+        data_dict = data.model_dump()
+        async with uow:
+            await uow.posts.edit_one(post_id, data_dict)
+
+            current_post = await uow.posts.find_one(id=post_id)
+            post_history_log = PostHistorySchemaAdd(
+                post_id=post_id,
+                previous_header=current_post.header,
+                previous_body=current_post.body,
+                new_header=data.header,
+                new_body=data.body,
+            )
+            post_history_log = post_history_log.model_dump()
+            await uow.post_history.add_one(post_history_log)
+            await uow.commit()
+
+    @staticmethod
+    async def get_post_history(uow: IUnitOfWork):
+        async with uow:
+            history = uow.post_history.find_all()
+            return history
+
+
